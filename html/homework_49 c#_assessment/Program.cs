9f08cba05cea231dee6733f333712d90de4859d8
ybeker-code <ybeker@woodmontcollege.edu>
2025-06-10 14:55:37 -0400

    homework 49

---
+++ html/homework_49 c#_assessment/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace c_Assessment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GetCloset();
+        }
+        public static void GetCloset()
+        {
+            List<Shirt> closet = new List<Shirt>();
+            string[] colors = { "red", "blue", "green" };
+            string[] patterns = { "striped", "checked", "plain" };
+            for (int i = 0; i < colors.Length; i++)
+            {
+                for (int j = 0; j < patterns.Length; j++)
+                {
+                    closet.Add(new Shirt(colors[i], patterns[j]));
+                }
+            }
+            Print(closet);
+        }
+        static void Print(List<Shirt> closet)
+        {
+            foreach (var item in closet)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ html/homework_49 c#_assessment/Shirt.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_Assessment
+{
+    internal class Shirt
+    {
+		private string color = "white";
+
+		public string Color
+        {
+			get { return color; }
+			set { color = value; }
+		}
+		private string pattern = "plain";
+
+		public string Pattern
+        {
+			get { return pattern; }
+			set { pattern = value; }
+		}
+
+        public Shirt(string color, string pattern)
+        {
+            Color = color;
+            Pattern = pattern;
+        }
+
+        public Shirt()
+        {
+        }
+
+        public override string? ToString()
+        {
+            return $"A {Color} {Pattern} shirt";
+        }
+    }
+}
