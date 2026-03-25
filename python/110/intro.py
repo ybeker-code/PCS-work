e1fcb92776908a30e08383ea4e030ec592d31cf3
ybeker-code <ybeker@woodmontcollege.edu>
2026-03-24 20:59:33 -0400

    homework 110

---
+++ python/110/intro.py
@@ -0,0 +1,16 @@
+x = 5
+print(x)
+Y: int | str
+Y = 10
+print(Y)
+Y = 'hello'
+print(Y)
+Y = True
+print(Y)
+Y = None
+print(Y)
+Y = 'hello'
+z = 'yehuda'
+print(Y + z)
+print((Y +' '+ z).upper())
+print(f'{z} is my name')
